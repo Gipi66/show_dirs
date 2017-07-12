bf0bbaabd2f0924f6ee5de1c887ec31b5e69efa0
sergey <martin10@bk.ru>
2017-07-12 10:13:33 +0300

    fix control flow for arguments checker

--- src/main/java/com/mobileffort/show_dirs/Main.java
+++ src/main/java/com/mobileffort/show_dirs/Main.java
@@ -7,9 +7,9 @@ public class Main {
 		if (args.length == 2) {
 			ViewManager vm = new ViewManager(args[0], args[1]);
 			vm.run();
-		} else if (args.length == 1 && args.equals("-help")) {
+		} else if (args.length == 1 && args[0].contains("-help")) {
 			out.println("The arguments must match the paths to the input file and the output file\n"
-					+ "For example:%njava -jar show_dirs.jar /home/in.txt /home/out.csv");
+					+ "For example:\njava -jar show_dirs.jar /home/in.txt /home/out.csv");
 		} else {
 			out.println("Argument error: To show help, use \"-help\"");
 		}
